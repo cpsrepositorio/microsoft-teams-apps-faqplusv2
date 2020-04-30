401c44b29bd0652313d03061fcf7674587ce6acc
Fabio Vello <fabio.vello@itcore.com.br>
2020-04-30 17:22:33 -0300

    ajuste date v3

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Properties/Strings.Designer.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Properties/Strings.Designer.cs
@@ -1060,7 +1060,7 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Properties {
         }
         
         /// <summary>
-        ///   Consulta uma cadeia de caracteres localizada semelhante a Data.
+        ///   Consulta uma cadeia de caracteres localizada semelhante a Date.
         /// </summary>
         public static string UpdateHistoryDateHeaderText {
             get {
